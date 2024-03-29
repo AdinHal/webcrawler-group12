7f1a7688e746e061729d46a2dd7ccbf1d3a7d19f
AdinHal <80127944+AdinHal@users.noreply.github.com>
2024-03-29 23:38:39 +0100

    Feature #3: Modified main class to match the assignment requirements, implemented additional user imports

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,29 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the URL:");
+        System.out.println("Please enter the URL:");
         String crawl_url = scanner.nextLine();
 
-        System.out.println("(Optional|Press Enter if You want to skip) Enter a language code:");
+        System.out.println("Please enter the Crawl depth");
+        int crawl_depth = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Please enter the domains to be crawled (comma-separated, no spaces): ");
+        String crawl_domains = scanner.nextLine();
+        List<String> crawlDomainsList = Arrays.asList(crawl_domains.split(","));
+
+        System.out.println("Please enter a language code (Optional* -> Press 'Enter' if You want to skip):");
         String crawl_lang = scanner.nextLine();
 
-        if(crawl_lang.length() == 0){
-            //Execute WebCrawler without Language Parameter
-        }else{
-            //Execute WebCrawler with Language Parameter
-        }
-
         scanner.close();
+
+
     }
 }
