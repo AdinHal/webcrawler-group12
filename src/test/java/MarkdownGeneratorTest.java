27501dcefb8465d3e962735a1d158f4e37cff061
AdinHal <80127944+AdinHal@users.noreply.github.com>
2024-04-29 20:39:23 +0200

    MarkdownGenerator WriteEntries Tests Written

--- src/test/java/MarkdownGeneratorTest.java
+++ src/test/java/MarkdownGeneratorTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 public class MarkdownGeneratorTest {
-    private static MarkdownGenerator markdownGenerator;
+
+    static MarkdownGenerator markdownGenerator;
 
     @Test
     public void testMDGeneratorInit() throws IOException {
@@ -44,6 +46,25 @@ public class MarkdownGeneratorTest {
        assertTrue("File should be created if non-existing",testFile.exists());
     }
 
+    @Test
+    public void testWriteEntriesWithEmptyString() throws IOException {
+        File testFile = Files.createTempDirectory("testDirectory").resolve("testOut.md").toFile();
+        markdownGenerator = new MarkdownGenerator(testFile.getAbsolutePath());
+
+        markdownGenerator.writeEntries("");
+
+        assertTrue("File is empty because an empty string was passed.",testFile.length() == 0);
+    }
+
+    @Test
+    public void testWriteEntriesWithNullString() throws IOException {
+        File testFile = Files.createTempDirectory("testDirectory").resolve("testOut.md").toFile();
+        markdownGenerator = new MarkdownGenerator(testFile.getAbsolutePath());
+
+        markdownGenerator.writeEntries(null);
+
+        assertTrue("File should be empty since an empty string was written.",testFile.length() == 0);
+    }
 
 
 }
