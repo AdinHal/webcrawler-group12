d1d0db7b744b0495e60f04a4b3caa7bb9838cc4b
D4NIFAKTOR1ELLE <d1medic@edu.aau.at>
2024-06-11 19:46:15 +0200

    Rewrote URL input validator

--- src/main/java/crawler/InputValidator.java
+++ src/main/java/crawler/InputValidator.java
@@ -1,20 +1,40 @@
 package crawler;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class InputValidator {
     public final Scanner scanner = new Scanner(System.in);
 
-    public String inputURLs(){
-        String URLRegEx = "\\b((https?|ftp):\\/\\/[^\\s,]+(?:\\/[^\\s,]*)?)\\b(?:,\\s*\\b((https?|ftp):\\/\\/[^\\s,]+(?:\\/[^\\s,]*)?)\\b)*";
+    public void splitURLs(){
+        String userInput = scanner.nextLine();
+        String[] urls;
 
-        Main.urlsToCrawl = scanner.nextLine();
-
-        while(!Main.urlsToCrawl.matches(URLRegEx)) {
-            System.out.print("One or more URLs are malformed. Please re-enter your URL(s): ");
-            Main.urlsToCrawl = scanner.next();
+        if (userInput.contains(",")) {
+            urls = userInput.split(",");
         }
-        return Main.urlsToCrawl;
+        else{
+            urls = new String[]{userInput};
+        }
+
+        inputURLs(urls);
+    }
+
+    public void inputURLs(String[] urls) {
+        try {
+            int index = 0;
+
+            for (String u : urls) {
+                new URL(u).toURI();
+                Main.urlsToCrawl[index] = u;
+                index++;
+            }
+        } catch (MalformedURLException | URISyntaxException e) {
+            System.out.print("One or more URLs are malformed or null. Try writing them again: ");
+            splitURLs();
+        }
     }
 
     public void inputDepth(){
--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -5,30 +5,24 @@ import java.util.*;
 public class Main {
     static Set<String> allowedDomains = new HashSet<>();
     public static int crawlDepth = 0;
-    public static String urlsToCrawl;
+    public static String[] urlsToCrawl = new String[100];
 
     public static void main(String[] args) {
-        try {
-            InputValidator IV = new InputValidator();
+        InputValidator IV = new InputValidator();
 
-            System.out.print("Enter the URLs to be crawled (comma separated): ");
-            //THIS IS FOR TESTING!
-            String urlsInput = "http://books.toscrape.com/";
-            //String urlsInput = IV.inputURLs();
+        System.out.print("Enter the URLs to be crawled (comma separated): ");
+        IV.splitURLs();
 
-            String[] urls = urlsInput.split(",");
+        System.out.print("Enter the crawling depth: ");
+        IV.inputDepth();
 
-            System.out.print("Enter the crawling depth: ");
-            IV.inputDepth();
+        System.out.print("Enter allowed domains (comma separated, press Enter to skip): ");
+        IV.inputDomains();
 
-            System.out.print("Enter allowed domains (comma separated, press Enter to skip): ");
-            IV.inputDomains();
+        System.out.println("\nTraversing site and writing file...\n");
+        WebCrawler.startExecutorService(urlsToCrawl, crawlDepth);
 
-            System.out.println("\nTraversing site and writing file...\n");
-            WebCrawler.startExecutorService(urls, crawlDepth);
-        } finally {
-            FileWriterSingleton.getInstance().close();
-            System.out.println("File written successfully.\n");
-        }
+        FileWriterSingleton.getInstance().close();
+        System.out.println("File written successfully.\n");
     }
 }
